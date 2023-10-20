fc2128b6a69db4e7cac9904ca9cb8ea9b273bad8
Jan Tschada <jts@esri-de.com>
2023-10-20 16:47:51 +0200

    Init measure module - MeasureTool added

---
+++ src/measure/tools.py
@@ -0,0 +1,5 @@
+class MeasureTool(object):
+    
+    def run(self, feature_class: str):
+        # TODO: Implement the add fields for 'speed, point_distance, point_direction'
+        pass
--- src/track_measure.py
+++ src/track_measure.py
@@ -2,6 +2,7 @@ from codecarbon import track_emissions
 from configparser import ConfigParser
 from glob import glob
 import logging
+from measure.tools import MeasureTool
 import os
 from traffic.read import read_sqlite_to_featureclass
 
@@ -10,7 +11,10 @@ from traffic.read import read_sqlite_to_featureclass
 @track_emissions(project_name="Urban Digital Twin Bonn - Measure", output_file="log/emissions.user", offline=True, country_iso_code="USA")
 def track_measure(traffic_filepath: str):
     traffic_featureclass = read_sqlite_to_featureclass(traffic_file_path, "SELECT * FROM agent_pos LIMIT 10;")
-    pass
+    measure_tool = MeasureTool()
+    measure_tool.run(traffic_featureclass)
+    #MeasureTool.run(traffic_featureclass)
+
 
 
 if __name__=="__main__":
