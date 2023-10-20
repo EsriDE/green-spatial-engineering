389dae778a1113cc78d71df1ae6f8b0493b86cdd
Jan Tschada <jts@esri-de.com>
2023-10-20 17:54:35 +0200

    Added the patterns module - structure only

---
+++ src/patterns/tools.py
@@ -0,0 +1,5 @@
+class HotColdSpotsTool(object):
+    
+    def run(self, feature_class: str):
+        # TODO: Implement the hot-cold spots analysis
+        pass
---
+++ src/track_patterns.py
@@ -0,0 +1,34 @@
+from codecarbon import track_emissions
+from configparser import ConfigParser
+from glob import glob
+import logging
+from patterns.tools import HotColdSpotsTool
+import os
+from traffic.read import read_sqlite_to_featureclass
+
+# config.user anpassen
+
+@track_emissions(project_name="Urban Digital Twin Bonn - Patterns", output_file="log/emissions.user", offline=True, country_iso_code="USA")
+def track_measure(traffic_filepath: str):
+    traffic_featureclass = read_sqlite_to_featureclass(traffic_file_path, "SELECT * FROM agent_pos LIMIT 10;")
+    hot_cold_spots_tool = HotColdSpotsTool()
+    hot_cold_spots_tool.run(traffic_featureclass)
+
+
+
+if __name__=="__main__":
+    logging.basicConfig()
+    logger = logging.getLogger("codecarbon")
+    
+    config = ConfigParser()
+    config.read("src/config.user")
+    
+    try:
+        traffic_file_path = config["DEFAULT"]["TrafficFilePath"]
+        if None is traffic_file_path:
+            raise ValueError("Traffic file path not specified!")
+        
+        track_measure(traffic_file_path)
+
+    except Exception as ex:
+        logging.getLogger("codecarbon").error(ex)
