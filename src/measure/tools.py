30da9a0dd00ac60c9874e89d827ccb4bd3c7e0b1
Thomas Knäuper <t.knaeuper@esri.de>
2023-10-23 14:57:22 +0200

    MeasureTool first steps

--- src/measure/tools.py
+++ src/measure/tools.py
@@ -1,5 +1,35 @@
+import arcpy
+import datetime
+
 class MeasureTool(object):
     
     def run(self, feature_class: str):
         # TODO: Implement the add fields for 'speed, point_distance, point_direction'
+
+        output_table = arcpy.AddFields_management(in_table=feature_class, field_description=[["point_direction", "DOUBLE", "", "", "0", ""], ["point_distance", "DOUBLE", "", "", "0", ""], ["speed", "DOUBLE", "", "", "0", ""]])
+
+
+        last_trip_id = int
+        last_longitude = float
+        last_latitude = float
+        last_time = 0
+
+        feature_class_column_names = ["trip", "longitude", "latitude", "trip_time", "point_direction", "point_distance", "speed"]
+        with arcpy.da.UpdateCursor(output_table, feature_class_column_names) as cur:
+
+            for row in cur:
+
+                current_trip_id = row[1]
+                current_longitude = row[0]
+                current_latitude = row[1]
+                current_time = datetime.datetime.timestamp(row[3])
+
+                if current_trip_id == last_trip_id:
+                    current_point = create_point_geometry(current_longitude, current_latitude)
+                    last_point = create_point_geometry(last_longitude, last_latitude)
+                    angle, distance = current_point.angleAndDistanceTo(last_point)
         pass
+
+    def round_datetime(dt):
+        delta = datetime.timedelta(seconds=5)
+        return datetime.datetime.min + round((dt - datetime.datetime.min) / delta) * delta
