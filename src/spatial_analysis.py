66c0721683fd46111613805ad290791eca7a7f1f
Alina Krämer <alkr@esri.de>
2023-10-30 15:34:12 +0100

    bugfixing in SpaceTimeCube Co-authored-by: Thomas Knäuper <t.knaeuper@esri.de>

--- src/spatial_analysis.py
+++ src/spatial_analysis.py
@@ -1,11 +1,7 @@
-from arcgis.features import GeoAccessor
-import json
-import logging
-import os
-import pandas as pd
-import sqlite3 as sql
-import warnings
-from measure import tools
+from traffic.read import read_sqlite_to_featureclass
+from measure.tools import MeasureTool
+from patterns.tools import HotColdSpotsTool, SpaceTimeCube
+
 
 def run(db_filepath: str, select_statement: str, space_time_cube_path: str, time_interval: int, distance_interval: int, output_geodatabase: str, x_column: str='longitude', y_column: str='latitude'):
 
@@ -18,5 +14,7 @@ def run(db_filepath: str, select_statement: str, space_time_cube_path: str, time
     space_time_cube.create_space_time_cube(feature_class, space_time_cube_path, time_interval=15, distance_interval=100)
     space_time_cube.visualize_space_time_cube(space_time_cube_path, output_geodatabase)
 
-    pass
+    hot_cold_spots = HotColdSpotsTool()
+    hot_cold_spots.create_hot_cold_spots_feature_class()
 
+    pass
