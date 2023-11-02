92a5cbf836c809c6f4c1648a36eb1b14461b908a
Alina Krämer <alkr@esri.de>
2023-11-02 14:49:37 +0100

    bugfixing

--- src/measure/tools.py
+++ src/measure/tools.py
@@ -36,11 +36,15 @@ class Trip(object):
 
 class MeasureTool(object):
     
-    def round_datetime(self, dt):
-        #dt = date time
-        delta = datetime.timedelta(seconds=5)
-        return datetime.datetime.min + round((dt - datetime.datetime.min) / delta) * delta
-    
+    def change_timefield(self, feature_class: str):
+        out_feature_class = arcpy.conversion.ExportFeatures(
+            in_features=feature_class,
+            out_features="memory/traffic_data_changed_timefield",
+            use_field_alias_as_name="NOT_USE_ALIAS",
+            field_mapping='id "id" true true false 0 Long 0 0,First,#,traffic_data,id,-1,-1;trip "trip" true true false 0 Long 0 0,First,#,traffic_data,trip,-1,-1;person "person" true true false 0 Long 0 0,First,#,traffic_data,person,-1,-1;vehicle_type "vehicle_type" true true false 8 Text 0 0,First,#,traffic_data,vehicle_type,0,8;distance_crossed "distance_crossed" true true false 0 Long 0 0,First,#,traffic_data,distance_crossed,-1,-1;latitude "latitude" true true false 0 Double 0 0,First,#,traffic_data,latitude,-1,-1;longitude "longitude" true true false 0 Double 0 0,First,#,traffic_data,longitude,-1,-1;trip_time_old "trip_time_old" true true false 38 Text 0 0,First,#,traffic_data,trip_time,0,38',)
+        
+        return out_feature_class[0]
+        
     def create_geometry(self, longitude, latitude):
 
         point = arcpy.Point(longitude, latitude)
@@ -52,20 +56,26 @@ class MeasureTool(object):
         
         trip_point_a = Trip.create_empty()
 
-        feature_class_column_names = ["trip", "longitude", "latitude", "trip_time", "point_direction", "point_distance", "speed"]
+        feature_class_column_names = ["trip", "longitude", "latitude", "trip_time", "trip_time_old", "point_direction", "point_distance", "speed"]
         with arcpy.da.UpdateCursor(feature_class, feature_class_column_names) as cur:
 
             for row in cur:
                 
-                trip_point_b = Trip("Point B", row[0], row[1], row[2], self.round_datetime(row[3]), float, float ,float)
+                timestamp_string = row[4]
+                
+                format_string = "%Y-%m-%dT%H:%M:%S"
+                datetime_object = datetime.datetime.strptime(timestamp_string, format_string)
+                row[3] = datetime.datetime.fromtimestamp(datetime.datetime.timestamp(datetime_object) - 3600)
+                
+                trip_point_b = Trip("Point B", row[0], row[1], row[2], row[3], float, float ,float)
                 if trip_point_b.equals(trip_point_a):
 
                     trip_point_b = self.calculate_distance_speed(trip_point_b, trip_point_a)
-                    row[4]=trip_point_b.angle
-                    row[5]=trip_point_b.distance
-                    row[6]=trip_point_b.speed
+                    row[5]=trip_point_b.angle
+                    row[6]=trip_point_b.distance
+                    row[7]=trip_point_b.speed
 
-                    cur.updateRow(row)
+                cur.updateRow(row)
 
                 trip_point_a = Trip("Point A", trip_point_b.trip_id, trip_point_b.long, trip_point_b.lat,
                                     trip_point_b.trip_time, trip_point_b.angle, trip_point_b.distance, trip_point_b.speed)
@@ -78,17 +88,24 @@ class MeasureTool(object):
         trip_point_b.angle, trip_point_b.distance = current_point.angleAndDistanceTo(last_point)
         
         differential_seconds = datetime.timedelta.total_seconds(trip_point_b.trip_time - trip_point_a.trip_time)
-        trip_point_b.speed = trip_point_b.distance/differential_seconds * 3.6
+
+        if trip_point_b.distance == 0:
+            trip_point_b.speed = 0
+        else:
+            trip_point_b.speed = trip_point_b.distance/differential_seconds * 3.6
+
+        trip_point_b.distance = round(trip_point_b.distance, 2)
 
         return trip_point_b
 
-    def run(self, feature_class: str, output_feature_class: str):
-        # TODO: Implement the add fields for 'speed, point_distance, point_direction'
-    
+    def run(self, feature_class: str):
+            
+        feature_class = self.change_timefield(feature_class)
+
         sorted_feature_class = "memory/traffic_data"
-        arcpy.Sort_management(feature_class, sorted_feature_class, [["trip", "ASCENDING"], ["trip_time", "ASCENDING"]])
-        feature_class = arcpy.AddFields_management(in_table=sorted_feature_class, field_description=[["point_direction", "DOUBLE", "", "", "0", ""], ["point_distance", "DOUBLE", "", "", "0", ""], ["speed", "DOUBLE", "", "", "0", ""]])
+        arcpy.Sort_management(feature_class, sorted_feature_class, [["trip", "ASCENDING"], ["trip_time_old", "ASCENDING"]])
+        feature_class = arcpy.AddFields_management(in_table=sorted_feature_class, field_description=[["trip_time", "DATE"],["point_direction", "DOUBLE", "", "", "0", ""], ["point_distance", "DOUBLE", "", "", "0", ""], ["speed", "DOUBLE", "", "", "0", ""]])
 
-        MeasureTool.measure(feature_class)
-
-        arcpy.CopyFeatures_management(feature_class, output_feature_class)
+        self.measure(feature_class)
+        arcpy.DeleteField_management(feature_class, drop_field=["trip_time_old", "ORIG_FID"])
+        return feature_class
--- src/patterns/tools.py
+++ src/patterns/tools.py
@@ -3,9 +3,9 @@ from sys import argv
 
 class SpaceTimeCube(object):
     
-    def create_space_time_cube(self, feature_class: str, output_geodatabase: str, space_time_cube_path: str, time_interval: int, distance_interval: int):
+    def create_space_time_cube(self, feature_class: str, space_time_cube_path: str, time_interval: int, distance_interval: int):
 
-        geodatabase_feature_class = arcpy.ExportFeatures_conversion(feature_class, f"{output_geodatabase}/trafficFeatureClass")
+        geodatabase_feature_class = arcpy.ExportFeatures_conversion(feature_class, "trafficFeatureClass")
 
         projected_feature_class = arcpy.Project_management(in_dataset = geodatabase_feature_class,
                                                            out_dataset = f"{geodatabase_feature_class}_projected_25832",
@@ -20,40 +20,32 @@ class SpaceTimeCube(object):
 
         return space_time_cube
 
-    def visualize_space_time_cube(self, space_time_cube_path:str, output_geodatabase:str):
+    def visualize_space_time_cube(self, space_time_cube_path:str):
 
         arcpy.VisualizeSpaceTimeCube3D_stpm(in_cube = space_time_cube_path,
                                             cube_variable = "COUNT",
                                             display_theme = "VALUE",
-                                            output_features = f"{output_geodatabase}/SpaceTimeCubeVisualize")
+                                            output_features = "SpaceTimeCubeVisualize")
 
 
 class HotColdSpotsTool(object):
 
-    def create_hot_cold_spots_space_time(space_time_cube_path: str, output_geodatabase:str, distance_interval: int):
+    def create_hot_cold_spots_space_time(self, space_time_cube_path: str, distance_interval: int):
     
         arcpy.stpm.EmergingHotSpotAnalysis(in_cube = space_time_cube_path,
                                            analysis_variable = "COUNT",
-                                           output_features = f"{output_geodatabase}/SpaceTimeCube_EmergingHotSpotAnalysis",
+                                           output_features = "SpaceTimeCube_EmergingHotSpotAnalysis",
                                            neighborhood_distance =  f"{distance_interval} Meters")
 
         arcpy.stpm.LocalOutlierAnalysis(in_cube = space_time_cube_path,
                                         analysis_variable = "COUNT",
-                                        output_features = f"{output_geodatabase}/SpaceTimeCube_LocalOutlierAnalysis",
+                                        output_features = "SpaceTimeCube_LocalOutlierAnalysis",
                                         neighborhood_distance =  f"{distance_interval} Meters")
         
-    def create_hot_cold_spots_feature_class(feature_class: str, output_geodatabase:str, distance_interval: int, time_interval: int):
-
-        # arcpy.FindPointClusters_gapro(input_points = feature_class,
-        #                               out_feature_class= f"{output_geodatabase}_PointCluster",
-        #                               clustering_method="DBSCAN",
-        #                               minimum_points= 100,
-        #                               search_distance= f"{distance_interval} Meters",
-        #                               use_time="TIME",
-        #                               search_duration=f"{time_interval} Minutes")
+    def create_hot_cold_spots_feature_class(self, feature_class: str, distance_interval: int, time_interval: int):
 
         arcpy.CalculateDensity_gapro(input_layer = feature_class,
-                                     out_feature_class = f"{output_geodatabase}_CalculateDensity",
+                                     out_feature_class = "CalculateDensity",
                                      bin_type = "HEXAGON",
                                      bin_size = f"{distance_interval} Meters",
                                      weight = "UNIFORM",
@@ -62,8 +54,33 @@ class HotColdSpotsTool(object):
                                      time_step_interval = f"{time_interval} Minutes")
         
         arcpy.FindHotSpots_gapro(point_layer = feature_class,
-                                 out_feature_class = f"{output_geodatabase}_FindHotSpots",
+                                 out_feature_class = "FindHotSpots",
                                  bin_size = f"{distance_interval} Meters",
                                  neighborhood_size = f"{distance_interval * 1.5} Meters",
                                  time_step_interval = f"{time_interval} Minutes")
         
+    
+class PatternsTool(object):
+# ToDO: config.user muss space_time_cube_path, time_interval, distance_interval enthalten 
+    def run(self, feature_class, space_time_cube_path, time_interval, distance_interval):
+        # Measure Tool
+        measure_tool = MeasureTool()
+        feature_class = measure_tool.run(feature_class)
+
+        # SpaceTimeCube Tool
+        space_time_cube_tool = SpaceTimeCube()
+        space_time_cube = space_time_cube_tool.create_space_time_cube(feature_class,
+                                                                      space_time_cube_path,
+                                                                      time_interval,
+                                                                      distance_interval)
+
+        # HotColdSpots Tool
+        hot_cold_spots_tool = HotColdSpotsTool()
+        hot_cold_spots_tool.create_hot_cold_spots_space_time(space_time_cube_path,
+                                                             distance_interval)
+
+        hot_cold_spots_tool.create_hot_cold_spots_feature_class(feature_class,
+                                                                distance_interval,
+                                                                time_interval)
+        
+        return space_time_cube
