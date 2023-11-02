dfa26c0d5b7a4d2b77edef745ce555e921c724f9
Alina Krämer <alkr@esri.de>
2023-11-02 17:46:10 +0100

    removed limit

--- src/track_measure.py
+++ src/track_measure.py
@@ -10,7 +10,7 @@ from traffic.read import read_sqlite_to_featureclass
 
 @track_emissions(project_name="Urban Digital Twin Bonn - Measure", output_file="log/emissions.user", offline=True, country_iso_code="USA")
 def track_measure(traffic_filepath: str):
-    traffic_featureclass = read_sqlite_to_featureclass(traffic_file_path, "SELECT * FROM agent_pos LIMIT 10;")
+    traffic_featureclass = read_sqlite_to_featureclass(traffic_file_path, "SELECT * FROM agent_pos;")
     workspace_dir = "/arcgis/home/traffic"
     
     measure_tool = MeasureTool()
