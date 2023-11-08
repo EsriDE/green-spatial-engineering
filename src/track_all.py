3183d34b1b7f657166458297a74b32b1ab6adb2a
Jan Tschada <jts@esri-de.com>
2023-11-08 08:12:53 +0100

    Added a track all script

---
+++ src/track_all.py
@@ -0,0 +1,28 @@
+from codecarbon import track_emissions
+from configparser import ConfigParser
+from glob import glob
+import logging
+from track_measure import track_measure
+from track_patterns import track_patterns
+
+
+
+if __name__=="__main__":
+    logging.basicConfig()
+    logger = logging.getLogger("codecarbon")
+    
+    config = ConfigParser()
+    config.read("src/config.user")
+    
+    execution_count = 10
+    try:
+        traffic_file_path = config["DEFAULT"]["TrafficFilePath"]
+        if None is traffic_file_path:
+            raise ValueError("Traffic file path not specified!")
+        
+        for _ in range(0, execution_count):
+            track_measure(traffic_file_path)
+            track_patterns()
+
+    except Exception as ex:
+        logging.getLogger("codecarbon").error(ex)
